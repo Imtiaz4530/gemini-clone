362920238bbb148e09d35684a65bf0b057a4c518
Imtiaz4530 <smimtiaz58@gmail.com>
2024-11-23 21:36:07 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Gemini clone - AI-Powered Chatbot
+
+This project is a **Gemini clone** that leverages advanced natural language processing (NLP) models to provide intelligent, conversational AI. Built using **Gemini API** , this application allows users to interact with AI for a variety of use cases such as casual conversation, problem-solving, and creative writing.
+
+## 🚀 Features
+
+- **AI-Powered Conversations**: Seamlessly interact with an AI model trained to understand and respond to text-based input.
+- **Multilingual Support**: The app is capable of understanding and responding in multiple languages, offering a versatile experience for global users.
+- **Real-Time Responses**: Get instant, intelligent responses to your questions and prompts.
+- **Customizable AI Models**: Allows easy integration with different language models for specialized tasks
+
+## 🔧 Technologies Used
+
+- **Frontend**: React
+- **AI API**: GEMINI
+
+## 🖼️ How It Works
+
+1. **User Input**: Users type their queries or prompts into the chat interface.
+2. **AI Processing**: The text is sent to the backend where the **GEMINI model** processes the input.
+3. **AI Response**: The AI generates a response based on the model's understanding of the query.
+4. **Output**: The response is displayed to the user in real-time on the frontend interface.
+
+## 📦 Installation & Setup
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/Imtiaz4530/gemini-clone
+   cd gemini-clone
+   npm install
+   npm run dev
+   ```
+
+## Contributing
+
+Contributions are welcome! Feel free to open issues or submit pull requests to improve the Gemini Clone.
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,65 @@
+import { useContext, useState } from "react";
+import { assets } from "../../assets/assets";
+import "./sidebar.css";
+import { Context } from "../../context/Context";
+
+const Sidebar = () => {
+  const [extended, setExtended] = useState(false);
+  const { onSent, previousPrompts, setRecentPrompt, newChat } =
+    useContext(Context);
+
+  const loadPrompt = async (prompt) => {
+    setRecentPrompt(prompt);
+    await onSent(prompt);
+  };
+
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <img
+          onClick={() => setExtended((p) => !p)}
+          className="menu"
+          src={assets.menu_icon}
+          alt=""
+        />
+        <div onClick={() => newChat()} className="new-chat">
+          <img src={assets.plus_icon} alt="" />
+          {extended ? <p>New Chat</p> : null}
+        </div>
+        {extended ? (
+          <div className="recent">
+            <p className="recent-title">Recent</p>
+            {previousPrompts.map((item, i) => {
+              return (
+                <div
+                  key={i}
+                  className="recent-entry"
+                  onClick={() => loadPrompt(item)}
+                >
+                  <img src={assets.message_icon} alt="" />
+                  <p>{item.slice(0, 18)} ...</p>
+                </div>
+              );
+            })}
+          </div>
+        ) : null}
+      </div>
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+          <img src={assets.question_icon} alt="" />
+          {extended ? <p>Help</p> : null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.history_icon} alt="" />
+          {extended ? <p>Activity</p> : null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.setting_icon} alt="" />
+          {extended ? <p>Settings</p> : null}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
